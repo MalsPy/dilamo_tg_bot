8398b348440e6a1225d610d1b9d62656538c00ad
MalsPy <woxa.github.123@gmail.com>
2025-04-23 09:38:56 +0500

    ready

--- common/texts_for_db.py
+++ common/texts_for_db.py
@@ -4,20 +4,20 @@ from aiogram.utils.formatting import Bold, as_list, as_marked_section
 categories = ['Мальчики','Девочки']
 
 description_for_info_pages = {
-    "main": "Добро пожаловать!",
-    "about": "Режим работы - круглосуточно.",
+    "main": "Dilamo kids\n Babyshoppinguz (старый аккаунт )\n Детская одежда из Турции и Китая\n",
+    "about": "• Принимаем заказы круглосуточно \n• Самарканд, ул. Узбекистанская (ориентир — 34-я школа)\n• Тел: +998 50 550 33 59\n Instagram:\nhttps://www.instagram.com/dilamikids?igsh=MWNnbTc3ZnFkNDloMw==",
     "payment": as_marked_section(
         Bold("Варианты оплаты:"),
-        "Картой в боте",
-        "При получении карта/кеш",
+        "• Наличными при получении",
+        "• Переводом на карту",
+        "• Переводом на карту Можно оплатить доставщику или скинуть на карту после получения товара",
         marker="✅ ",
     ).as_html(),
     "shipping": as_list(
         as_marked_section(
-            Bold("Варианты доставки/заказа:"),
-            "Курьер",
-            "Самовынос",
-            "UzPost",
+            Bold("Доставка по Узбекистану:"),
+            "• По Самарканду — TaxiOK, Яндекс, доставщик",
+            "• По регионам — почта BTS и EMU",
             marker="✅ ",
         ),
         sep="\n----------------------\n",
